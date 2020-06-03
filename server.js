136d054031f9b5fcd0e1acbc9870d4a947f14c83
Souvik <souvikde.ns@gmail.com>
2020-06-04 02:43:30 +0530

    created .gitignore file

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require('./app')
 const mongoose = require('mongoose')
 
+/*
+* Connecting mongoose
+* mongoose needs a mongodb url to connect,
+* spin up your mongoose database and add
+* the mongoose url inside the .env file.
+* */
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -10,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL,{
     console.log(err)
 })
 
+// Starting the server
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started at port ${process.env.PORT || 3000}`)
 })
