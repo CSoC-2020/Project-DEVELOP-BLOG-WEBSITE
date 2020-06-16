0727ce5751890b6ed27646d4974a417b06c9b0d1
Souvik <souvikde.ns@gmail.com>
2020-06-16 12:39:48 +0530

    cleaning project

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = mongoose.Schema({
     username: {
@@ -12,38 +13,35 @@ const userSchema = mongoose.Schema({
     }
 })
 
+
+userSchema.pre('save', function (next) {
+    var user = this;
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password along with our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// For Verification
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        return cb(null, isMatch)
+    });
+}
+
+
 const User = mongoose.model('user', userSchema)
 
-const createUser = async (username, password) => {
-
-    try {
-        let hashedPassword = await bcrypt.hash(password, 12)
-
-        const user = new User({
-            username: username,
-            password: hashedPassword
-        })
-
-        await user.save()
-
-        return user
-    } catch (e) {
-        return throw e
-    }
-}
-
-const comparePassword = async (user, password) => {
-    try {
-        const result = await bcrypt.compare(user.password, password)
-        return result
-
-    } catch (e) {
-        throw e
-    }
-}
-
-module.exports = {
-    User,
-    createUser,
-    comparePassword
-}
